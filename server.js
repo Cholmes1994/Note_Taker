e5527cb71d8dd97b6a90592320d6e198c104bbf9
Christopher Holmes <holmes.chris94@yahoo.com>
2020-08-12 13:30:05 -0400

    Created my server

---
+++ server.js
@@ -0,0 +1,22 @@
+// Require express
+const express = require("express");
+
+// Create an express server
+const app = express();
+
+// Sets the port
+const PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+// Routes
+require("./routes/htmlRoutes")(app);
+require("./routes/apiRoutes")(app);
+
+// Server listening
+app.listen(PORT, function () {
+    console.log("App listening on PORT: " + PORT);
+});
